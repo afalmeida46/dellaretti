2a0e19bd221e2f03a2b5415ff78e96710bc28d14
afalmeida46 <132354379+afalmeida46@users.noreply.github.com>
2026-05-27 13:54:53 -0300

    Update main.tsx

--- artifacts/dellaretti/src/main.tsx
+++ artifacts/dellaretti/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { installDevtoolsBlocker } from "./lib/devtools-block";
-
-installDevtoolsBlocker();
 
 createRoot(document.getElementById("root")!).render(<App />);
